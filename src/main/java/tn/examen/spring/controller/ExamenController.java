694da29b271626f532720d88fca91b3a614d8516
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2023-01-12 23:18:14 +0100

    KAFKA

--- src/main/java/tn/examen/spring/controller/ExamenController.java
+++ src/main/java/tn/examen/spring/controller/ExamenController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
 
-import io.swagger.annotations.ApiOperation;
+
 
 import tn.examen.spring.entities.Evenement;
 import tn.examen.spring.entities.Logistique;
