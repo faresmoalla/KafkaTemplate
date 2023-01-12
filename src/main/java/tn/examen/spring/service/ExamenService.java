d54f36a9cc6bf857346766608958baee4cb7dd01
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2023-01-12 23:22:30 +0100

    KAFKA

--- src/main/java/tn/examen/spring/service/ExamenService.java
+++ src/main/java/tn/examen/spring/service/ExamenService.java
@@ -1,11 +1,11 @@
 package tn.examen.spring.service;
 
 import java.util.ArrayList;
-import java.util.Date;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
+
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
